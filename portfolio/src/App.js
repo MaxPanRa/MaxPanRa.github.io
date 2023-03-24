9c069c256edc3a655ff38bc941abdbce4d883c36
Max Pantoja <maxpanra@gmail.com>
2023-03-24 13:18:58 -0600

    Cambios Snapping en la Web

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
     return(
       <div>
         <div class="black-bg">
-          <ScrollContainer snap="">
+          <ScrollContainer >
             <ScrollPage>
             <Animator animation={FadeInZoomIn}>
               <div className="container-wallpaper white-shine" id="page-0">
