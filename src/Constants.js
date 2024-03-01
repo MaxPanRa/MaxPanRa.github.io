590bee469f0399c56247364c735637aa3bc93fa2
maximiliano.pantoja <maximiliano.pantoja@gtec.com.mx>
2024-03-01 14:49:58 -0600

    Cambios CLIENT GUID OAUTH

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,5 @@
-//export const CLIENT_GUID='561326193392-71u84huibkpfu36g6hfmfdlnc5hg781v.apps.googleusercontent.com';
-export const CLIENT_GUID='jcxxgpnztjRVjCvjCMkk';
+export const CLIENT_GUID='561326193392-71u84huibkpfu36g6hfmfdlnc5hg781v.apps.googleusercontent.com';
+//export const CLIENT_GUID='jcxxgpnztjRVjCvjCMkk';
 export const LOOKER_WEB= "https://gtechdev.cloud.looker.com/";
 export const REDIRECT_URI='https://maxpanra.github.io/'
 //export const REDIRECT_URI='http://localhost:3000'
