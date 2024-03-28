ceba4270468ee944ec23b48665c3f430d92750ab
maximiliano.pantoja <maximiliano.pantoja@gtec.com.mx>
2024-03-28 00:20:30 -0600

    k

--- src/Components/Recommendations/Recommendations.js
+++ src/Components/Recommendations/Recommendations.js
@@ -103,9 +103,9 @@ class Recommendations extends Component {
 
         let htmlElem = (
             recommended.map((x,k)=>
-            <div style={{fontSize:"10px !important",width:"300px"}} className={x.vm_forecast_dash_obs_cliente+"-recom prod-txt"}>{x.vm_forecast_dash_PRODUCTO+ " Producto #"+x.vm_forecast_dash_row_num}
+            <div style={{fontSize:"10px !important",width:"100%"}} className={x.vm_forecast_dash_obs_cliente+"-recom prod-txt"}>{x.vm_forecast_dash_PRODUCTO+ " Producto #"+x.vm_forecast_dash_row_num}
                 {x.suggestions.map((y,l)=>
-                    <span style={{fontSize:"10px !important",width:"200px"}} key={l} className="suggestion" >{"Cambiar por: "+y.p_PRODUCTO}</span>
+                    <span style={{fontSize:"10px !important",width:"100%"}} key={l} className="suggestion" >{"Cambiar por: "+y.p_PRODUCTO}</span>
                 )}
             </div>
             )
